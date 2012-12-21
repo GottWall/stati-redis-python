faa480bd2b46b791c03adb9c3fa5b50f30977447
Alex <Alexandr.S.rus@gmail.com>
2012-12-21 11:44:15 +0400

    New RedisClient logic

--- stati/client.py
+++ stati/client.py
@@ -10,10 +10,14 @@ Client for GottWall
 :license: BSD, see LICENSE for more details.
 :github: http://github.com/Lispython/stati-python
 """
-
+import logging
 import json
 import datetime
 import redis
+
+
+logger = logging.getLogger('stati')
+
 
 class Client(object):
     """Base client
@@ -59,18 +63,45 @@ class RedisClient(Client):
         self._password = password
         self._db = db
         self._pool = None
+        self._key = None
+        self._channel = None
+        self._connection_pool = None
+        self._redis = None
 
     @property
     def channel(self):
         """Make channel name
         """
-        return "gottwall:{0}:{1}:{2}".format(self._project, self._public_key, self._private_key)
+        if not self._channel:
+            self._channel = "gottwall:{0}:{1}:{2}".format(self._project, self._public_key, self._private_key)
+        return self._channel
+
+    @property
+    def data_key(self):
+        """Make key that store
+        """
+        if not self._key:
+            self._key = "gottwall:{0}:{1}".format(self._project, self._public_key, self._private_key)
+        return self._key
+
+    @property
+    def redis(self):
+        if not self._connection_pool:
+            self._connection_pool = redis.ConnectionPool(host=self._host, port=self._port,
+                                                     db=self._db, password=self._password)
+        if not self._redis:
+            self._redis = redis.Redis(connection_pool=self._connection_pool)
+
+        return self._redis
 
     def incr(self, name, timestamp=datetime.datetime.now(), value=1,
              filters={}):
-        pool = redis.ConnectionPool(host=self._host, port=self._port,
-                                    db=self._db, password=self._password)
-        r = redis.Redis(connection_pool=pool)
+        try:
+            redis = self.redis
 
-        r.publish(self.channel, self.serialize(name, timestamp, value, filters))
+            redis.sadd(self.data_key, self.serialize(name, timestamp, value, filters))
+            redis.publish(self.channel, json.dumps({"type": "notification"}))
+        except Exception, e:
+            print(e)
+            logger.warn(e)
 
--- tests/client.py
+++ tests/client.py
@@ -22,10 +22,7 @@ private_key = "gottwall_pricatekey"
 public_key = "project_public_key"
 project = "test_gottwall_project"
 
-redis = Redis(host=HOST)
-check_pubsub = redis.pubsub()
-check_pubsub.psubscribe("gottwall:*")
-next(check_pubsub.listen())
+
 
 class ClientTestCase(BaseTestCase):
 
@@ -51,26 +48,40 @@ class ClientTestCase(BaseTestCase):
 
 
 class RedisClientTestCase(BaseTestCase):
+
+    def setUp(self):
+        self.redis = Redis(host=HOST)
+        self.redis.flushdb()
+
+    def tearDown(self):
+        self.redis.flushdb()
+
     def test_pubsub(self):
+
+        check_pubsub = self.redis.pubsub()
+        check_pubsub.psubscribe("gottwall:*")
+        next(check_pubsub.listen())
 
         cli = RedisClient(
             private_key=private_key,
             public_key=public_key,
             project=project,
             host=HOST)
-
         ts = datetime.utcnow()
 
         self.assertEquals("gottwall:{0}:{1}:{2}".format(project, public_key, private_key), cli.channel)
-
         cli.incr(name="orders", value=2, timestamp=ts, filters={"current_status": "Completed"})
-
 
         message = next(check_pubsub.listen())
 
         self.assertEquals(message['channel'], 'gottwall:{0}:{1}:{2}'.format(project, public_key, private_key))
 
-        data_dict = json.loads(message['data'])
+        notification_message = json.loads(message['data'])
+        self.assertEquals(notification_message['type'], 'notification')
+
+        data_dict = json.loads(self.redis.spop(cli.data_key))
         self.assertTrue(data_dict['name'], 'orders')
         self.assertTrue(data_dict['timestamp'], ts.strftime("%Y-%m-%dT%H:%M:%S"))
         self.assertTrue(data_dict['filters']['current_status'], 'Completed')
+
+        self.assertEquals(self.redis.scard(cli.data_key), 0)
